c5955d77b4b4bf4ba655604872eb2e4ca788d878
vitkristof <kisviti006@gmail.com>
2025-10-02 23:31:28 +0200

    Delete things
    don't need

--- Title_Screen_Backup/Scripts/Game_1.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-
-func _on_quit_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Games.tscn")
-	pass # Replace with function body.
--- Title_Screen_Backup/Scripts/Settings.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-func _ready() -> void:
-	$menu/Quit.grab_focus()
-	
-func _on_quit_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/TitleScreen.tscn")
-	
